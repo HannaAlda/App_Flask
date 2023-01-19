b0e986ca6fa61be6536c4f4ffdcc781b57014ecd
Elvia Aldana Hernández <63755965+HannaAlda@users.noreply.github.com>
2023-01-19 13:29:39 -0600

    Primeras pruebas

--- index.py
+++ index.py
@@ -1,19 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-"""
-@app.route('/')
-def principal():
-    # return "Bienvenido o bienvenida a mi sitio web con python!"
-    return "UskoKruM2010 - Suscríbete!"
-
-@app.route('/contacto')
-def contacto():
-    return "Esta es la página de contacto"
-"""
-
-
 @app.route('/')
 def principal():
     return render_template('index.html')
